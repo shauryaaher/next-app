f0e33c53c1500c1c3cc18e5defe2a8db2b485a39
Shaurya Aher <shauryaaher1200@gmail.com>
2022-12-25 15:55:25 +0530

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+import styles from "../styles/Home.module.css";
+
+const home = () => {
+  return (
+    <>
+      <div className={styles.main}>
+        <h1>Hello world</h1>
+      </div>
+    </>
+  );
+};
+
+export default home;
